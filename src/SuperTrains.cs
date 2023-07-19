71c0e6f27a5f8bbd452b25c1e7fa4337e7a57909
Alkinesio <alkinesio2001@gmail.com>
2023-07-19 14:09:37 +0200

    Trying to link code part [Unstable vers.]
    Trying to link code part.

---
+++ src/SuperTrains.cs
@@ -0,0 +1,15 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+
+namespace SuperTrains
+{
+    public class SuperTrainsMod : ModSystem
+    {
+
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+        }
+    }
+}
